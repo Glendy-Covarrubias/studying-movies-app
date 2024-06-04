4f200d6751040a9f9891cd26140d1957769d90ae
Glendy-Covarrubias <glendycovarrubias@hotmail.com>
2024-06-03 18:31:19 -0600

    Pruebas

--- BlazorPeliculas/obj/Debug/net8.0/BlazorPeliculas.AssemblyInfo.cs
+++ BlazorPeliculas/obj/Debug/net8.0/BlazorPeliculas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorPeliculas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5e1835254db20fed50db7cf5274d5718d24a466")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorPeliculas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorPeliculas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
